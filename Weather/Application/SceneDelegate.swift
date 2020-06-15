29a92f7e4b5fd4255f9a29b91136cd5b19fb642a
Lucas Firmo <lucas.firmo@corp.globo.com>
2020-06-15 18:25:42 -0300

    chore: arrange folders structure

---
+++ Weather/Application/SceneDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  SceneDelegate.swift
+//  Weather
+//
+//  Created by lucas.firmo on 6/15/20.
+//  Copyright © 2020 domene. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = HomeViewController()
+        window?.makeKeyAndVisible()
+    }
+
+}
+
---
+++ Weather/Flows/Home/HomeViewController.swift
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.swift
+//  Weather
+//
+//  Created by lucas.firmo on 6/15/20.
+//  Copyright © 2020 domene. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+}
